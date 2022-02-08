8c09b2b6e03d84597ffbdc42c51aa6cea6fe0842
Caio Santos <caiosantosfreitas@hotmail.com>
2022-02-08 20:40:30 -0300

    feat: design layout with bootstrap

--- WebApp/src/app/app.module.ts
+++ WebApp/src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/people/components/header/header.component';
+import { ListPeopleComponent } from './pages/people/components/list-people/list-people.component'
+import { PeopleFormComponent } from './pages/people/components/people-form/people-form.component';
+import { DeletePeopleComponent } from './pages/people/components/delete-people/delete-people.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ListPeopleComponent,
+    PeopleFormComponent,
+    DeletePeopleComponent
   ],
   imports: [
     BrowserModule,
---
+++ WebApp/src/app/pages/people/components/delete-people/delete-people.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-people',
+  templateUrl: './delete-people.component.html',
+  styleUrls: ['./delete-people.component.css']
+})
+export class DeletePeopleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
